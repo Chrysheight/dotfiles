52792411495a5faa54b09b79909d356fd882750f
Chrysheight <jeos1492@gmail.com>
2026-01-16 12:49:09 -0300

    Feat: changing roslyn for omnisharp

---
+++ lua/plugins/omnisharp.lua
@@ -0,0 +1,16 @@
+return {
+  'neovim/nvim-lspconfig',
+  opts = {
+    servers = {
+      omnisharp = {
+        cmd = { 'omnisharp' },
+        enable_editorconfig_support = true,
+        enable_ms_build_load_projects_on_demand = false,
+        enable_roslyn_analyzers = true,
+        organize_imports_on_format = true,
+        enable_import_completion = true,
+        sdk_include_prereleases = true, -- IMPORTANT for .NET 9
+      },
+    },
+  },
+}
