d25ad4f3c9f91bc2396a8e6dfd45cf587ea2a133
Chrysheight <jeos1492@gmail.com>
2026-01-15 23:54:32 -0300

    Roslyn plugin added

--- init.lua
+++ init.lua
@@ -201,20 +201,16 @@ rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-
-  -- NOTE: Plugins can also be added by using a table,
+  -- NOTE: Plugins can be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
   --
   -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
-  --
+  { import = 'plugins' },
 
-  opts = {
-    { import = 'plugis' },
-    { import = 'custom.plugins' },
-  },
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
   --    {
